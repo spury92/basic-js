f990cfab08e248c37f80e267dab759104c29827c
spury92 <bot1412@yandex.ru>
2020-02-23 21:40:33 +0300

    add solutions to carbon dating and count cats

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,16 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(SAMPLE_ACTIVITY) {
+  try {
+    SAMPLE_ACTIVITY = parseFloat(SAMPLE_ACTIVITY.replace(/[^0-9.-]*/, ''))
+  } catch(TypeError) {
+    return false;
+  }
+  if (!SAMPLE_ACTIVITY || SAMPLE_ACTIVITY <= 0 || SAMPLE_ACTIVITY >= 15) {
+    return false;
+  }
+  let k = 0.693 / HALF_LIFE_PERIOD;
+  // const k = -0.00012094240837696334;
+  return Math.ceil(Math.log(MODERN_ACTIVITY / SAMPLE_ACTIVITY) / k);
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,13 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  let cat = "^^"
+  let count = 0
+  matrix.map(array => {array.map(subarray => {
+    if (cat == subarray) {
+      count = count + 1;
+    }
+  }
+  )
+  }
+  )
+  return count;
 };
