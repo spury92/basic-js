2854d6ca068920520e427c0cd7c5a41bbafd353a
spury92 <bot1412@yandex.ru>
2020-02-29 16:03:05 +0300

    refactor: add solutions to dream-team and what-season

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,13 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let DreamTeam = []
+  if ( !members | !Array.isArray(members) ) {
+    return false
+  }
+  members.map(member => {
+    if (typeof member == 'string') {
+      DreamTeam.push(member.replace(/\s/g, '')[0].toUpperCase())
+    }
+  }
+  )
+  return DreamTeam ? DreamTeam.sort().join('') : false
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,23 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function getSeason(date) {
+  if (!date) {
+    return 'Unable to determine the time of year!'
+  }
+  if (date.hasOwnProperty('toString')) {
+    throw new Error()
+  }
+  let month = date.getMonth();
+
+  if ( month > 10) {
+    return 'winter'
+  } else if (month > 7) {
+    return 'fall'
+  } else if (month > 4) {
+    return 'summer'
+  } else if (month > 1) {
+    return 'spring'
+  } else {
+    return 'winter'
+  }
+  
+    
+}
